828f1985159de3ff15e2fc497e3309367b7db1ff
Martin Castillo <58961096+waskull@users.noreply.github.com>
2025-10-22 15:29:17 -0400

    Update settings.py

--- auth-service/auth_service/settings.py
+++ auth-service/auth_service/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -142,3 +142,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
